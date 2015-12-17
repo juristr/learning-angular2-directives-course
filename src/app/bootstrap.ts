356c1c92a191917b77679e5c0561d63a3e62d8c8
juristr <juri.strumpflohner@gmail.com>
2015-12-17 23:46:14 +0100

    use ES6 class syntax

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,11 +1,15 @@
-var Person = function (name) {
-	this.name = name;	
-}
-
-Person.prototype.registerSayHello = function() {
-	document.getElementById('someButton').addEventListener('click', () => {
-		console.log('Hi, I am a ' + this + ' and my name is ' + this.name);
-	});
+class Person {
+	
+	constructor(name) {
+		this.name = name;
+	}
+	
+	registerSayHello() {
+		document.getElementById('someButton').addEventListener('click', () => {
+			console.log('Hi, I am a ' + this + ' and my name is ' + this.name);
+		});
+	}
+	
 }
 
 var p = new Person('Juri');
