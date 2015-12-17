f0b734ba3aaba09fdd5e00d4f2b5b66d52402539
juristr <juri.strumpflohner@gmail.com>
2015-12-17 23:44:55 +0100

    simple ES5 class person

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,1 +1,14 @@
-console.log('app loaded');
+var Person = function (name) {
+	this.name = name;	
+}
+
+Person.prototype.registerSayHello = function() {
+	document.getElementById('someButton').addEventListener('click', function() {
+		console.log('Hi, I am a ' + this + ' and my name is ' + this.name);
+	});
+}
+
+var p = new Person('Juri');
+p.registerSayHello();
+
+document.getElementById('someButton').click();
