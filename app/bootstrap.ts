d914468d4304e39af56492b702ef5a7a08b82308
juristr <juri.strumpflohner@gmail.com>
2016-02-09 13:22:27 +0100

    add ELEMENT_PROBE_PROVIDERS for debugging support

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -4,14 +4,19 @@ import { provide } from 'angular2/core';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
+
+// For Batarangle Integration
+import {ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
+
 import {App} from './app.component';
 
 bootstrap(App, [
         ...HTTP_PROVIDERS,
         ...ROUTER_PROVIDERS,
-        provide(LocationStrategy, { useClass: HashLocationStrategy })
+        provide(LocationStrategy, { useClass: HashLocationStrategy }),
+        ...ELEMENT_PROBE_PROVIDERS
     ])
     .then(function(){
-        materialLite.componentHandler.upgradeAllRegistered();        
+        materialLite.componentHandler.upgradeAllRegistered();
     })
 
