0bbf8fe6b9014ebe9c4d92784e22f0e06d4b9185
juristr <juri.strumpflohner@gmail.com>
2016-02-20 18:55:14 +0100

    change to path based routing

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-// import {bootstrap} from 'angular2/platform/browser';
-import {bootstrap} from 'angular2-universal-preview';
+import {bootstrap} from 'angular2/platform/browser';
+// import {bootstrap} from 'angular2-universal-preview';
 import * as materialLite from 'material-design-lite/material';
 import { provide } from 'angular2/core';
 import { HTTP_PROVIDERS } from 'angular2/http';
-import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {ROUTER_PROVIDERS, LocationStrategy } from 'angular2/router';
+import {APP_BASE_HREF} from 'angular2/router';
 
 
 // For Batarangle Integration
@@ -14,12 +15,13 @@ import {App} from './app.component';
 bootstrap(App, [
         ...HTTP_PROVIDERS,
         ...ROUTER_PROVIDERS,
-        provide(LocationStrategy, { useClass: HashLocationStrategy }),
+        // provide(LocationStrategy, { useClass: HashLocationStrategy }),
+        provide(APP_BASE_HREF, {useValue : '/' }),
         ...ELEMENT_PROBE_PROVIDERS
     ])
     .then(function(){
         console.log('running materialLite handlers');
-        materialLite.componentHandler.upgradeAllRegistered();
+        // materialLite.componentHandler.upgradeAllRegistered();
     })
     .catch(err => console.error(err));
 
