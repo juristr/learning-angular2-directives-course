bb6c61ff75a4925aafc8028ebeacea449dc0e7f6
juristr <juri.strumpflohner@gmail.com>
2016-03-23 15:56:16 +0100

    add static sensor list

--- app/sensor-config/sensor-config.component.ts
+++ app/sensor-config/sensor-config.component.ts
@@ -1,8 +1,33 @@
 import {Component, OnInit} from 'angular2/core';
 
+interface Sensor {
+    name: string,
+    description: string
+}
+
 @Component({
     selector: 'configure',
     templateUrl: 'app/sensor-config/sensor-config.html'
 })
 
-export class SensorConfigureComponent { }
+export class SensorConfigureComponent {
+    private sensors: Sensor[];
+
+    constructor() {
+        this.sensors = [
+            {
+                name: "S1-001XX",
+                description: "Temperature and humidity sensor"
+            },
+            {
+                name: "S1-002XX",
+                description: "Temperature and humidity sensor"
+            },
+            {
+                name: "S1-003XX",
+                description: "Temperature and humidity sensor"
+            }
+        ]
+    }
+
+ }
