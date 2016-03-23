b36d297a3f5127cfbff86c8527b3fe92075bbbc1
juristr <juri.strumpflohner@gmail.com>
2016-03-23 09:12:38 +0100

    extract header component

--- app/app.ts
+++ app/app.ts
@@ -1,32 +1,14 @@
 import {Component} from 'angular2/core';
 
+import { HeaderCmp } from './header';
+
 @Component({
   selector: 'app',
+  directives: [ HeaderCmp ],
   template: `
     <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
-        <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
-            <div class="mdl-layout__header-row">
-                <span class="mdl-layout-title">Home</span>
-                <div class="mdl-layout-spacer"></div>
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-                    <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
-                        <i class="material-icons">search</i>
-                    </label>
-                    <div class="mdl-textfield__expandable-holder">
-                        <input class="mdl-textfield__input" type="text" id="search">
-                        <label class="mdl-textfield__label" for="search">Enter your query...</label>
-                    </div>
-                </div>
-                <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
-                    <i class="material-icons">more_vert</i>
-                </button>
-                <ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" for="hdrbtn">
-                    <li class="mdl-menu__item">About</li>
-                    <li class="mdl-menu__item">Contact</li>
-                    <li class="mdl-menu__item">Legal information</li>
-                </ul>
-            </div>
-        </header>
+        <app-header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
+        </app-header>
         <div class="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
             <header class="demo-drawer-header">
                 <img src="assets/images/user.jpg" class="demo-avatar">
