25a0b7f69bd20d7902ce426ce78ebdf6e5bc6a97
juristr <juri.strumpflohner@gmail.com>
2016-03-23 15:33:59 +0100

    add sensor-config component

---
+++ app/sensor-config/sensor-config.component.ts
@@ -0,0 +1,8 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'configure',
+    templateUrl: 'app/sensor-config/sensor-config.html'
+})
+
+export class SensorConfigureComponent { }
