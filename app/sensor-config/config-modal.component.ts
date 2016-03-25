590c61fa88dbf85eae2ffa8b423133ce9762d6eb
juristr <juri.strumpflohner@gmail.com>
2016-03-25 11:24:40 +0100

    add 1st skeleton of modal dialog

---
+++ app/sensor-config/config-modal.component.ts
@@ -0,0 +1,22 @@
+import {Component } from 'angular2/core';
+
+@Component({
+    selector: 'config-modal',
+    template: `
+        <div class="md-dialog mdl-color--white mdl-shadow--2dp">
+            <div class="md-dialog-content">
+
+            </div>
+            <div class="md-dialog-actions">
+
+            </div>
+        </div>
+        <div class="md-backdrop"></div>
+    `
+})
+
+export class ConfigModalComponent {
+
+    constructor() { }
+
+}
