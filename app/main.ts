21eb5c03d05f609600fcecc13444d96aa4a489ea
juristr <juri.strumpflohner@gmail.com>
2016-05-26 00:11:27 +0200

    finish angular RC.1 upgrade

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { provide } from '@angular/core';
-import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from '@angular/router-deprecated';
+import {ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { HTTP_PROVIDERS } from '@angular/http';
 
 import * as materialLite from 'material-design-lite';
